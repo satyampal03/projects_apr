08e5ffda99f77aa61154ec976fc4ee997a5a004b
Satyam Pal <em.satyam@gmail.com>
2026-04-14 14:28:28 +0400

    Minor File Changes

---
+++ 6_prac/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+
+const app = express(); // handler function
+const fs = require('fs');
+
+
+
+
+
+app.use(express.urlencoded({extended: false}));
+
+
+app.get("/users", (req, res) => {
+  const html = `
+        <ul>
+            ${users.map((user) => `<li>${user.first_name}</li>`).join('')}
+        </ul>
+    `;
+  res.send(html);
+});
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+// geting only 1 user usoing id
+app.get('/api/users/:id', (req,res)=>{
+    const paraId = Number(req.params.id);
+    const user = users.find((user) =>  user.id === paraId);
+
+    if (!user) return res.status(404).json({error: "User Not Found"}); 
+   return  res.json(user);
+})
+
+app.post('/api/user', (req,res)=>{
+    const body = req.body;
+    
+    users.push({...body, id:users.length+1});
+     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (error) => {
+        if (error) {
+            return res.status(500).json({ status: "error", message: "Failed to write file" });
+        }
+        return res.json({ status: 'successfully pushed', id:users.length});
+    });
+}) 
+
+app.patch('/api/user:id', (req,res)=>{
+    
+    const userUpdateId = Number(req.params.id);
+    const body = req.body;
+
+    const userIndex = users.find((user)=>user.id === userUpdateId);
+
+    if(userIndex === -1) {
+        return req.status(404).json({error:'User Not Found'});
+    }
+
+    users[userIndex] = {...users[userIndex], ...body};
+
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (error) => {
+        if (error) {
+            return res.status(500).json({ status: "error", message: "Failed to write file" });
+        }
+        return res.json({ status: 'successfully pushed', id:users.length});
+    });
+
+}) 
+
+app.delete('/api/user', (req,res)=>{
+        return res.json({status : 'pending'});
+}) 
+
+
+// Server is Running PORT
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
